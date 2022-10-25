a7b1a66b8c9b2e2b8adc854cca7c885d6da3b354
Greg Underwood <gunderwood@tractionguest.com>
2022-10-24 22:51:56 -0700

    fixed deploy script to work with main as the default branch

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:LarryLeonard5/LarryLeonard5.github.io.git master:gh-pages
+git push -f git@github.com:LarryLeonard5/LarryLeonard5.github.io.git main:gh-pages
 
 cd -
